f68918ab5eadccf66f26c89d9a003caafa6cebf3
AnimatorJoe <josephjin7@gmail.com>
2020-05-02 17:07:31 -0500

    granting admin permissions to those who request it

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,48 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp()
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// give admin permissions to another user
+exports.addAdminUser = functions.https.onCall((data, context) => {
+  const email = data.email
+
+});
+
+
+// give admin permissions to user requesting for permissions, if that user is authorized
+exports.grantAdminPermissions = functions.https.oncall((data, context) => {
+  const email = data.email
+  if (context.auth.token.admin) {
+    return {
+      result: "User ${email} is already given admin permissions."
+    };
+  }
+  setAdmin(email).then(() => {
+    return {
+      result: "User ${email} has been given admin permissions."
+    };
+  });
+});
+
+// set a given user as an admin
+async function setAdmin(email) {
+  const user = await admin.auth().getUserByEmail(req);
+  return admin.auth.setCustomUserClaims(user.uid, {
+    admin = true,
+  });
+}
+
+// determine whether a user has permission to gain admin access
+async function isAuthorized(email) {
+
+}
+
+// set user as being authorized
+async function makeAuthorized(email) {
+
+}
+
+// Create and Deploy Your First Cloud Functions
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});

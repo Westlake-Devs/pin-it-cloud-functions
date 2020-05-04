b898ec38879607716f36c43224809cd880190b61
AnimatorJoe <josephjin7@gmail.com>
2020-05-03 19:06:22 -0500

    granting admin permissions based on internal config

--- functions/index.js
+++ functions/index.js
@@ -1,40 +1,51 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-admin.initializeApp()
+admin.initializeApp();
+
+const db = admin.firestore();
+const adminsDoc = db.doc("/root/internal/user-groups/admins");
 
 // give admin permissions to another user
 exports.addAdminUser = functions.https.onCall((data, context) => {
-  const email = data.email
-
 });
 
 
 // give admin permissions to user requesting for permissions, if that user is authorized
-exports.grantAdminPermissions = functions.https.oncall((data, context) => {
-  const email = data.email
+exports.grantAdminPermissions = functions.https.onCall(async (data, context) => {
+  const email = context.auth.token.email;
   if (context.auth.token.admin) {
-    return {
-      result: "User ${email} is already given admin permissions."
-    };
+    return { result: `User ${email} is already given admin permissions.` };
   }
-  setAdmin(email).then(() => {
-    return {
-      result: "User ${email} has been given admin permissions."
-    };
-  });
+
+  let authorized = await isAuthorized(email);
+  if (!authorized) {
+    throw new functions.https.HttpsError('permission-denied', `User ${email} is not authorized to use admin permissions.`);
+  }
+
+  await setAdmin(email);
+  return { result: `User ${email} has been given admin permissions.` };
 });
 
 // set a given user as an admin
 async function setAdmin(email) {
-  const user = await admin.auth().getUserByEmail(req);
-  return admin.auth.setCustomUserClaims(user.uid, {
-    admin = true,
+  const user = await admin.auth().getUserByEmail(email);
+  await admin.auth().setCustomUserClaims(user.uid, {
+    admin: true,
   });
 }
 
-// determine whether a user has permission to gain admin access
+// determine whether a user is authorized to gain admin access
 async function isAuthorized(email) {
-
+  let doc = await adminsDoc.get();
+  if(!doc.exists) {
+    let err = "admin doc is missing.";
+    throw(err);
+  }
+  else {
+    const dat = doc.data();
+    console.log(`retrieved admins doc:\n${dat}`);
+    return email in dat;
+  }
 }
 
 // set user as being authorized
